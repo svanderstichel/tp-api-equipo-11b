726babf45e926f979d8beac0adcea114b17b3959
svanderstichel <vanderstichel@gmail.com>
2025-10-08 19:10:22 -0300

    Se creo el controlador de producto, incluyendo las referencias a dominio y negocio

---
+++ TPAPI_equipo-11b/api-productos/Controllers/ProductoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using dominio;
+using negocio;
+
+namespace api_productos.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        // GET: api/Producto
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Producto/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Producto
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Producto/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Producto/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
