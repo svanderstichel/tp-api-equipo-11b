6b76ea09bbf609eea753440a792098dce4dcf08a
caromgan <cmgandin@live.com>
2025-10-14 22:45:46 -0300

    modificacion de producto por API

--- TPAPI_equipo-11b/negocio/ArticuloNegocio.cs
+++ TPAPI_equipo-11b/negocio/ArticuloNegocio.cs
@@ -111,7 +111,39 @@ namespace negocio
                 }
             }
         }
+
+        public void Modificar(Articulo articulo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("UPDATE ARTICULOS SET Codigo = @codigo, Nombre = @nombre, Descripcion = @descripcion, IdMarca = @idmarca, IdCategoria = @idcategoria, Precio = @precio WHERE Id = @id");
+                //atos.setearConsulta("UPDATE ARTICULOS SET Codigo = @codigo, Nombre = @nombre, Descripcion = @descripcion, IdMarca = @idmarca, IdCategoria = @idcategoria, Precio = @precio WHERE Id = @id");
+                datos.setearParametro("@codigo", articulo.CodigoArticulo);
+                datos.setearParametro("@nombre", articulo.Nombre);
+                datos.setearParametro("@descripcion", articulo.Descripcion);
+                datos.setearParametro("@idmarca", articulo.Marca.IdMarca);
+                datos.setearParametro("@idcategoria", articulo.Categoria.IdCategoria);
+                datos.setearParametro("@precio", articulo.Precio);
+                datos.setearParametro("@id", articulo.IdArticulo);
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
+
+
+
 }
+
 
 
