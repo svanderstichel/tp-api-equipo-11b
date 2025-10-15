fb09e3c2f8a67a0531c6e27262441da6cf85c690
svanderstichel <vanderstichel@gmail.com>
2025-10-15 13:46:15 -0300

    Se añadieron validaciones y http responses a los metodos post y put.

--- TPAPI_equipo-11b/api-productos/Controllers/ProductoController.cs
+++ TPAPI_equipo-11b/api-productos/Controllers/ProductoController.cs
@@ -16,9 +16,8 @@ namespace api_productos.Controllers
         // GET: api/Producto
        public IEnumerable<Articulo> Get()
         {
-            ArticuloNegocio articulo = new ArticuloNegocio();
-            return articulo.ListarArticulos();
-
+                ArticuloNegocio articulo = new ArticuloNegocio();
+                return articulo.ListarArticulos();
         }
         
 
@@ -29,53 +28,103 @@ namespace api_productos.Controllers
         }
 
         // POST: api/Producto
-        public void Post([FromBody]ProductoDto producto)
+        public HttpResponseMessage Post([FromBody]ProductoDto producto)
         {
             Articulo aux = new Articulo();
-            
-            aux.CodigoArticulo = producto.CodigoArticulo;
-            aux.Nombre = producto.Nombre;
-            aux.Descripcion = producto.Descripcion;
-            aux.Precio = producto.Precio;
-            aux.Marca = new Marca() { IdMarca = producto.idMarca };
-            aux.Categoria = new Categoria() { IdCategoria = producto.idCategoria };
 
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            negocio.AltaArticulo(aux);
+            //validar que exista la marca y categoria
+            CategoriaNegocio validarCategoria = new CategoriaNegocio();
+            if((validarCategoria.Buscar(producto.idCategoria)).IdCategoria==0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "La categoria no existe.");
+
+            MarcaNegocio validarMarca = new MarcaNegocio();
+            if ((validarMarca.Buscar(producto.idMarca)).IdMarca == 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "La Marca no existe.");
+
+            try
+            {
+                aux.CodigoArticulo = producto.CodigoArticulo;
+                aux.Nombre = producto.Nombre;
+                aux.Descripcion = producto.Descripcion;
+                aux.Precio = producto.Precio;
+                aux.Marca = new Marca() { IdMarca = producto.idMarca };
+                aux.Categoria = new Categoria() { IdCategoria = producto.idCategoria };
+
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                negocio.AltaArticulo(aux);
+                return Request.CreateResponse(HttpStatusCode.OK, "Producto agregado correctamente.");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Ocurrió un error inesperado.");
+            }
         }
 
         // POST: api/Producto/id
-        public void Post(int id, [FromBody] ImagenesDto imagenes)
+        public HttpResponseMessage Post(int id, [FromBody] ImagenesDto imagenes)
         {
             Articulo aux = new Articulo();
 
-            aux.IdArticulo = id;
-            aux.ListaImagenes = new List<Imagen>();
-            foreach (var imagen in imagenes.ListaImagenes)
+            //validar que exista el articulo
+            ArticuloNegocio validarArticulo= new ArticuloNegocio();
+            if ((validarArticulo.Buscar(id)).IdArticulo == 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "El articulo no existe.");
+
+            try
             {
-                aux.ListaImagenes.Add(new Imagen(id, imagen));
+                aux.IdArticulo = id;
+                aux.ListaImagenes = new List<Imagen>();
+                foreach (var imagen in imagenes.ListaImagenes)
+                {
+                    aux.ListaImagenes.Add(new Imagen(id, imagen));
+                }
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                negocio.CargarImagenes(aux);
+                return Request.CreateResponse(HttpStatusCode.OK, "Imagenes agregadas correctamente.");
             }
 
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            negocio.CargarImagenes(aux);
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Ocurrió un error inesperado.");
+            }
         }
         // PUT: api/Producto/id
 
-       public void Put(int id, [FromBody] ProductoDto art)
+       public HttpResponseMessage Put(int id, [FromBody] ProductoDto art)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            Articulo modifica = new Articulo();
-            modifica.CodigoArticulo = art.CodigoArticulo;
-            modifica.Nombre = art.Nombre;
-            modifica.Descripcion = art.Descripcion;
-            modifica.Marca = new Marca { IdMarca = art.idMarca };
-            modifica.Categoria = new Categoria { IdCategoria = art.idCategoria };
-            modifica.Precio = art.Precio;
-           
-            
-            
-            modifica.IdArticulo = id;
-            negocio.Modificar (modifica);
+            //validar que exista el articulo
+            ArticuloNegocio validarArticulo = new ArticuloNegocio();
+            if ((validarArticulo.Buscar(id)).IdArticulo == 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "El articulo no existe.");
+
+            //validar que exista la marca y categoria
+            CategoriaNegocio validarCategoria = new CategoriaNegocio();
+            if ((validarCategoria.Buscar(art.idCategoria)).IdCategoria == 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "La categoria no existe.");
+
+            MarcaNegocio validarMarca = new MarcaNegocio();
+            if ((validarMarca.Buscar(art.idMarca)).IdMarca == 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "La Marca no existe.");
+
+            try
+            {
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                Articulo modifica = new Articulo();
+                modifica.CodigoArticulo = art.CodigoArticulo;
+                modifica.Nombre = art.Nombre;
+                modifica.Descripcion = art.Descripcion;
+                modifica.Marca = new Marca { IdMarca = art.idMarca };
+                modifica.Categoria = new Categoria { IdCategoria = art.idCategoria };
+                modifica.Precio = art.Precio;
+
+                modifica.IdArticulo = id;
+                negocio.Modificar(modifica);
+                return Request.CreateResponse(HttpStatusCode.OK, "Articulo modificado correctamente.");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Ocurrió un error inesperado.");
+            }
 
         }
         
---
+++ TPAPI_equipo-11b/negocio/CategoriaNegocio.cs
@@ -0,0 +1,41 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class CategoriaNegocio
+    {
+        public Categoria Buscar(int id)
+        {
+            Categoria categoria = new Categoria();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearParametro("@id", id);
+                datos.setearConsulta("SELECT Id, Descripcion FROM CATEGORIAS WHERE Id = @id");
+                datos.ejercutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    categoria.IdCategoria = (int)datos.Lector["Id"];
+                    categoria.Nombre = (string)datos.Lector["Descripcion"];
+                }
+
+                return categoria;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+    }
+}
